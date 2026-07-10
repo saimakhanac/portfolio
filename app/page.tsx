0d74cd13a93f822efaf66b22bbeb8779eea6fcce
saimakhanac <saimakhan0zy0@gmail.com>
2026-07-10 18:51:35 +0530

    skill section

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <Header />
       <Hero />
       <AboutMeSection />
-      <ContactUs />
       <SkillsSection />
+      <ContactUs />
       <Footer />
     </>
   );
--- components/skillsSection.tsx
+++ components/skillsSection.tsx
@@ -1,5 +1,32 @@
 "use client";
+import { title } from "process";
 import React, { useEffect, useRef, useState } from "react";
+import { CodeXml, Layers, LibraryIcon, Package } from "lucide-react";
+import {
+  SiCypress,
+  SiDocker,
+  SiFigma,
+  SiGit,
+  SiGraphql,
+  SiJavascript,
+  SiJest,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiPython,
+  SiReact,
+  SiSass,
+  SiStorybook,
+  SiTailwindcss,
+  SiTypescript,
+  SiVercel,
+  SiVuedotjs,
+  SiWebpack,
+} from "react-icons/si";
+import { FaAws } from "react-icons/fa";
+import { TbApi } from "react-icons/tb";
+import type { IconType } from "react-icons";
 const skillCategories = [
   {
     title: "Frontend",
@@ -29,6 +56,62 @@ const skillCategories = [
     ],
   },
 ];
+const techStack = [
+  {
+    title: "Programming",
+    icon: CodeXml,
+    items: ["JavaScript", "TypeScript", "Python"],
+  },
+  {
+    title: "Frameworks",
+    icon: Layers,
+    items: ["React", "Next.js", "Vue.js", "Node.js"],
+  },
+  {
+    title: "Libraries",
+    icon: LibraryIcon,
+    items: ["Tailwind CSS", "GraphQL", "REST APIs", "Sass", "Jest", "Cypress"],
+  },
+  {
+    title: "Packages & Tools",
+    icon: Package,
+    items: [
+      "PostgreSQL",
+      "MongoDB",
+      "Git",
+      "Docker",
+      "AWS",
+      "Vercel",
+      "Figma",
+      "StoryBook",
+      "Webpack",
+    ],
+  },
+];
+const techIcon: Record<string, IconType> = {
+  JavaScript: SiJavascript,
+  TypeScript: SiTypescript,
+  Python: SiPython,
+  React: SiReact,
+  "Next.js": SiNextdotjs,
+  "Vue.js": SiVuedotjs,
+  "Node.js": SiNodedotjs,
+  "Tailwind CSS": SiTailwindcss,
+  GraphQL: SiGraphql ,
+  "REST APIs": TbApi,
+  Sass: SiSass,
+  Jest: SiJest,
+  Cypress: SiCypress,
+  PostgreSQL: SiPostgresql,
+  MongoDB: SiMongodb,
+  Git: SiGit,
+  Docker: SiDocker,
+  Vercel: SiVercel,
+  Figma: SiFigma,
+  StoryBook: SiStorybook ,
+  Webpack: SiWebpack,
+  AWS: FaAws,
+};
 
 const SkillsSection = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -69,24 +152,70 @@ const SkillsSection = () => {
             {skillCategories.map((category, categoryIndex) => (
               <div className="bg-card rounded-xl p-8 mx-4" key={categoryIndex}>
                 <p>{category.title}</p>
-                {category.skills.map((skill,skillIndex) => (
+                {category.skills.map((skill, skillIndex) => (
                   <div className="my-6" key={skillIndex}>
                     <div key={skillIndex} className="flex justify-between">
                       <p>{skill.name}</p>
                       <p>{skill.level}%</p>
                     </div>
                     <div className="h-2 bg-secondary rounded-full">
-                      <div className="h-full bg-linear-to-r from-primary to-primary/60 rounded-full transition-all duration-1000 ease-out" style={{width:isVisible?`${skill.level}%`:0}}/>
+                      <div
+                        className="h-full bg-linear-to-r from-primary to-primary/60 rounded-full transition-all duration-1000 ease-out"
+                        style={{ width: isVisible ? `${skill.level}%` : 0 }}
+                      />
                     </div>
                   </div>
                 ))}
               </div>
             ))}
-            
-            <div></div>
-            <div></div>
           </div>
           {/* box end */}
+
+          {/* tech stack start */}
+          <div>
+            <div className="capitalize text-lg font-semibold text-foreground text-center">
+              Technology stack
+            </div>
+          </div>
+
+          <div className="lg:grid grid-cols-2 gap-6 my-6">
+            {techStack.map((list, index) => {
+              const TitleIcon = list.icon;
+              return (
+                <div
+                  className="bg-card p-6 rounded-md border border-border hover:border-primary/10"
+                  key={index}
+                >
+                  <div className="flex gap-4 items-center">
+                    <div className="bg-primary/10 text-primary rounded-md w-8 h-8 flex items-center justify-center">
+                      <TitleIcon size={18} />
+                    </div>
+                    <div className="text-base text-foreground font-semibold">
+                      {list.title}
+                    </div>
+                  </div>
+                
+                  {/* chip */}
+                  <div className="flex flex-wrap gap-2 my-2">
+                    {list.items.map((item, index) => {
+                      const TechIcon = techIcon[item];
+                      return (
+                        <div
+                          key={index}
+                          className="mt-1 py-1 px-2 flex gap-2.5 bg-secondary dark:bg-primary dark:text-background font-semibold items-center rounded-full text-xs text-foreground"
+                        >
+                          <div>{TechIcon && <TechIcon />}</div>
+                          <div>{item}</div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          {/* tech start ends here */}
         </div>
       </section>
     </>
